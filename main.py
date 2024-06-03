01466421f0aa3bdbd899b65ee10cb89ef94ce0f5
Ramapriya Rangaraju <sephirothrr@gmail.com>
2024-06-02 21:03:32 -0400

    version 1

---
+++ main.py
@@ -0,0 +1,107 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import json
+import os
+import pbds
+from types import SimpleNamespace
+
+tournament = 'nsc2023'
+
+teams = {}
+pools = []
+games = []
+
+
+def get_games(tournament):
+    games = []
+    for f in os.listdir(f'data/{tournament}/games'):
+        if f.endswith('.json'):
+            with open(f'data/{tournament}/games/{f}', 'r') as file:
+                parsed = f.replace('.json', " ").split("_")
+                parsed[1] = parsed[1].replace("-", " ")
+                parsed[2] = parsed[2].replace("-", " ")
+                data = json.load(file)
+                rnd = int(parsed[0])
+                tuh = data['tossups_read']
+                team1 = data['match_teams'][0]['team']['name']
+                team1tuscore = 0
+                team2tuscore = 0
+                team1bscore = data['match_teams'][0]['bonus_points']
+                team2bscore = data['match_teams'][1]['bonus_points']
+
+                for player in data['match_teams'][0]['match_players']:
+                    buzzes = player['answer_counts']
+                    if len(buzzes):
+                        for buzz in buzzes:
+                            team1tuscore += (buzz['answer']['value'] * buzz['number'])
+
+                for player in data['match_teams'][1]['match_players']:
+                    buzzes = player['answer_counts']
+                    if len(buzzes):
+                        for buzz in buzzes:
+                            team2tuscore += (buzz['answer']['value'] * buzz['number'])
+
+                team2 = data['match_teams'][1]['team']['name']
+                games.append(pbds.Game(rnd, tuh, team1, team1tuscore, team1bscore, team2, team2tuscore, team2bscore))
+    return games
+
+
+def get_teams(games, roundstart, roundend):
+    teams = {}
+    for game in games:
+        if game.round < roundstart or game.round > roundend:
+            continue
+        if game.team1 not in teams:
+            teams[game.team1] = pbds.Team(game.team1)
+        if game.team2 not in teams:
+            teams[game.team2] = pbds.Team(game.team2)
+        teams[game.team1].games += 1
+        teams[game.team2].games += 1
+        teams[game.team1].tuh += game.tuh
+        teams[game.team2].tuh += game.tuh
+        teams[game.team1].tupoints += game.team1tuscore
+        teams[game.team1].bpoints += game.team1bscore
+        teams[game.team2].tupoints += game.team2tuscore
+        teams[game.team2].bpoints += game.team2bscore
+        if game.team1score > game.team2score:
+            teams[game.team1].wins += 1
+            teams[game.team2].losses += 1
+        elif game.team1score < game.team2score:
+            teams[game.team2].wins += 1
+            teams[game.team1].losses += 1
+    return dict(sorted(teams.items()))
+
+def get_pools(tournament, prefix, teams):
+    pools = []
+    with open(f'data/{tournament}/setup/{prefix}pools.txt', 'r') as file:
+        poolnames = file.read().splitlines()
+        for p in poolnames:
+            pools.append(pbds.Pool(p))
+    with open(f'data/{tournament}/setup/{prefix}assignments.txt', 'r') as file:
+        assignments = file.read().splitlines()
+        for a in assignments:
+            team, pool = a.split('\t')[0], a.split('\t')[1]
+            pools[poolnames.index(pool)].teams.append(teams[team])
+    return pools
+
+
+if __name__ == '__main__':
+    games = get_games(tournament)
+    teams = get_teams(games, 1, 5)
+    pools = get_pools(tournament, "prelim", teams)
+    output = ""
+    for pool in pools:
+        if len(pool.teams):
+           output += f"{pool.name}\n"
+           for status in pool.findStatus:
+               output += status
+               output += "\n"
+        output += "\n"
+    print("Done!")
+    with open(f'data/{tournament}/output.txt', 'w') as file:
+        file.write(output)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ pbds.py
@@ -0,0 +1,112 @@
+from enum import Enum
+from itertools import groupby
+
+
+def ordinal(n):
+    return "%d%s" % (n, "tsnrhtdd"[(n // 10 % 10 != 1) * (n % 10 < 4) * n % 10::4])
+
+
+class Status(Enum):
+    UNCHECKED = "Status for {1}:{0} has not been checked."
+    PENDING = "{} is tentatively in {} place at {}-{} but still has score sheet(s) pending or a tied game pending a protest."
+    TWO_WAY_TIEBREAK = "{} and {} are tied for {} place at {}-{} and will need to play a one-stage tiebreaker."
+    THREE_WAY_TIEBREAK = "{}, {}, and {} are tied for {} place at {}-{} and will need to play a two-stage tiebreaker."
+    FOUR_ACROSS_TWO = "{}, {}, {}, and {} are tied for {} place at {}-{} and will need to play a one-stage tiebreaker."
+    FOUR_ACROSS_THREE = "{}, {}, {}, and {} are tied for {} place at {}-{} and will need to play a two-stage tiebreaker."
+    COMPLETE = "{} is free to go, in {} place at {}-{}."
+    ERROR = "Something went wrong with {1}:{0}'s status."
+
+
+# TODO: get real data model from qbj files once they are available
+
+class Team:
+    def __init__(self, name: str):
+        self.name = name
+        self.games = 0
+        self.wins = 0
+        self.losses = 0
+        self.tupoints = 0
+        self.tuh = 0
+        self.bpoints = 0
+        self.position = -1
+        self.status = Status.UNCHECKED
+
+
+class Game:
+    def __init__(self, rnd: int, tuh: int, team1: str, team1tuscore: int, t1bscore:int, team2: str, team2tuscore: int, t2bscore:int):
+        self.round = rnd
+        self.tuh = tuh
+        self.team1 = team1
+        self.team1tuscore = team1tuscore
+        self.team1bscore = t1bscore
+        self.team1score = team1tuscore + t1bscore
+        self.team2 = team2
+        self.team2tuscore = team2tuscore
+        self.team2bscore = t2bscore
+        self.team2score = team2tuscore + t2bscore
+
+
+class Pool:
+    def __init__(self, name: str):
+        self.name = name
+        self.teams = []
+        self._status = []
+
+    @property
+    def findStatus(self):
+        self.teams.sort(key=lambda x: (x.wins, x.tupoints + x.bpoints), reverse=True)
+        breaks = []
+        records = []
+        for k, g in groupby(self.teams, key=lambda x: (x.wins, x.losses)):
+            breaks.append(list(g))
+            records.append(k)
+        status = []
+        rank = 1
+
+        for i, brk in enumerate(breaks):
+            block_status = Status.UNCHECKED
+
+            if brk[0].games != 5:
+                block_status = Status.PENDING
+            else:
+                match len(brk):
+                    case 1:
+                        block_status = Status.COMPLETE
+                    case 2:
+                        if rank % 2:
+                            block_status = Status.TWO_WAY_TIEBREAK
+                        else:
+                            block_status = Status.COMPLETE
+                    case 3:
+                        block_status = Status.THREE_WAY_TIEBREAK
+                    case 4:
+                        if rank % 2:
+                            block_status = Status.FOUR_ACROSS_TWO
+                        else:
+                            block_status = Status.FOUR_ACROSS_THREE
+                    case _:
+                        block_status = Status.ERROR
+
+            for t in range(rank, rank + len(brk)):
+                self.teams[t - 1].status = block_status
+                self.teams[t - 1].position = rank
+
+            if block_status in (Status.UNCHECKED, Status.ERROR):
+                for team in brk:
+                    status.append(block_status.value.format(team.name, ordinal(rank)))
+            elif block_status in (Status.PENDING, Status.COMPLETE):
+                for team in brk:
+                    status.append(block_status.value.format(team.name, ordinal(rank), records[i][0], records[i][1]))
+            else:
+                status.append(block_status.value.format(*[t.name for t in brk], ordinal(rank), records[i][0], records[i][1]))
+
+            rank += len(brk)
+
+        self._status = status
+        return status
+
+
+class Tournament:
+    def __init__(self, name: str, pools: list[Pool]):
+        self.name = name
+        self.pools = pools
